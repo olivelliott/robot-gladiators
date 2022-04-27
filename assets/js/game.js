3637b4c01775bdf194877d18bdc94fc17b69851a
Olive <olivelliott48@gmail.com>
2022-04-27 19:38:43 -0400

    Add shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 10;
+var playerAttack = 60;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
@@ -39,34 +39,84 @@ var fight = function(enemyName) {
             window.alert(enemyName + " has died! ");
             playerMoney = playerMoney + 20;
             break;
-        }
-
-        else {
+        } else {
             window.alert(enemyName + " still has " + enemyHealth + " health remaining. ");
-        }
-
-        console.log(enemyName + " attacked " +  playerName  + " . " + playerName + " now has " + playerHealth + " health remaining. ");
-
-         if (playerHealth <= 0) {
+        } console.log(enemyName + " attacked " +  playerName  + " . " + playerName + " now has " + playerHealth + " health remaining. ");
+            if (playerHealth <= 0) {
             window.alert(playerName + " has died! ");
             break;
-        }
-
-        else {
+        } else {
             window.alert(playerName + " still has " + playerHealth + " health remaining. ");
         }
     }
-}
+};
 
-for(var i = 0; i < enemyNames.length; i++) {
+var startGame = function() {
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            playerHealth = 100;
+            playerAttack = 60;
+            playerMoney = 10;
+            window.alert("Welcome to Robot Gladiators! Round "+ (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+                if (storeConfirm) {
+                shop();
+                }
+            }
+        }
+    }
+};
+
+var endGame = function () {
     if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round "+ (i + 1));
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("You've lost your robot in battle.");
+    }var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-    else {
-        window.alert("You have lost your robot in battle! Game over!");
-    }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
+};
+
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE'");
+        switch (shopOptionPrompt) {
+            case "refill":
+            case "REFILL":
+                if (playerMoney >= 7) {
+                    window.alert("Refilling a player's health by 20 for 7 dollars");
+                    playerHealth = playerHealth + 20;
+                    playerMoney = playerMoney - 7;
+                } else {window.alert("You don't have enough money!");
+            } break;
+            case "upgrade":
+            case "UPGRADE":
+                if (playerMoney >= 7) {
+                    window.alert("Upgrading a player's attack by 6 for 7 dollars");
+                    playerAttack = playerAttack + 6;
+                    playerMoney = playerMoney - 7;
+                } else {window.alert ("You dont' have enough money!");
+            } break;
+            case "leave":
+            case "LEAVE":
+                window.alert("Leaving the store.");
+                break;
+            default:
+                window.alert("You did not pick a valid option. Try again.");
+                shop();
+                break; 
+        }
+
+};
+
+startGame();
+endGame();
+
 
