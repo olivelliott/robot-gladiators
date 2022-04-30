11cb0a2b2dcac3a43b2e3598b9660b56440a1739
Olive <olivelliott48@gmail.com>
2022-04-29 22:52:44 -0400

    Add number value for shop options

--- assets/js/game.js
+++ assets/js/game.js
@@ -90,18 +90,16 @@ var endGame = function () {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE'");
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL,  2 for UPGRADE,  or 3 for LEAVE");
+        shopOptionPrompt = parseInt (shopOptionPrompt);
         switch (shopOptionPrompt) {
-            case "refill":
-            case "REFILL":
+            case 1:
                 playerInfo.refillHealth();
                 break;
-            case "upgrade":
-            case "UPGRADE":
+            case 2:
                 playerInfo.upgradeAttack();
                 break;
-            case "leave":
-            case "LEAVE":
+            case 3:
                 window.alert("Leaving the store.");
                 break;
             default:
@@ -110,6 +108,8 @@ var shop = function() {
                 break;
         }
 };
+
+
 
 var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max-min + 1) + min);
